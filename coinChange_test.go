28f5f48429e2667d1e2da9968454b5ff3185c4b4
Itnotf <1096364408@qq.com>
2022-06-20 01:02:10 +0800

    solved coin change with dp. Leetcode 322

---
+++ coinChange_test.go
@@ -0,0 +1,13 @@
+package playWithDataStructures
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_coinChange(t *testing.T) {
+	assert.Equal(t, 3, coinChange([]int{1, 2, 5}, 11))
+	assert.Equal(t, -1, coinChange([]int{2}, 3))
+	assert.Equal(t, 0, coinChange([]int{1}, 0))
+}
