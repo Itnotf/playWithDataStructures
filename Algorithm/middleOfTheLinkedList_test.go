8b8de51e2bdef19ca2a9f92c8cb98299a7c73930
Itnotf <1096364408@qq.com>
2022-06-20 16:05:56 +0800

    solved middle of the linked list with two pointers Leetcode 876

---
+++ Algorithm/middleOfTheLinkedList_test.go
@@ -0,0 +1,11 @@
+package algorithm
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_middleNode(t *testing.T) {
+	assert.Equal(t, (*ListNode)(nil), middleNode(nil))
+	//todo
+}
