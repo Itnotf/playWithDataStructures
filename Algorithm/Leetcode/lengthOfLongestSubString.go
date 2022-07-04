40485c3fb47b7f25ddcc2947dbf98ce4d58daabc
Itnotf <1096364408@qq.com>
2022-07-04 23:51:29 +0800

    optimization length of longest sub string Leetcode 3

--- Algorithm/Leetcode/lengthOfLongestSubString.go
+++ Algorithm/Leetcode/lengthOfLongestSubString.go
@@ -1,27 +1,32 @@
 package Leetcode
 
 func lengthOfLongestSubstring(s string) int {
-	var window [256]int
-	i, j, longest := 0, 0, 0
-	for i < len(s) {
-		keyI := s[i]
-		window[keyI]++
+	//双指针i,j
+	//当前窗口map
+	//最长的长度maxLen
+	//如果s[i]不在窗口内，将s[i]加入窗口，maxLen = max(len(windows),maxLen)，i++
+	//如果s[i]在窗口中，
+	// -- 如果s[i] == s[j]，将s[j]移出窗口，j++ ,跳出循环
+	// -- 如果s[i] != s[j]，将s[j]移出窗口，j++，继续循环
 
-		if window[keyI] > 1 {
-			keyJ := s[j]
-			for keyJ != keyI {
-				window[keyJ]--
-				j++
-				keyJ = s[j]
-			}
-			window[keyJ]--
-			j++
-		} else {
-			if longest < i-j+1 {
-				longest = i - j + 1
-			}
+	i, j, maxLen := 0, 0, 0
+	windows := map[byte]int{}
+
+	for i < len(s) {
+		chi := s[i]
+		if _, ok := windows[chi]; !ok {
+			windows[chi] = 1
+			maxLen = max(i-j+1, maxLen)
+			i++
+			continue
 		}
-		i++
+
+		for s[i] != s[j] {
+			delete(windows, s[j])
+			j++
+		}
+		delete(windows, s[j])
+		j++
 	}
-	return longest
+	return maxLen
 }
